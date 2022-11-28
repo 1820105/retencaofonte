4b94bb9108b604320ed3748e2673a66303989510
1820105 <114646394+1820105@users.noreply.github.com>
2022-11-28 14:34:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ function calcSal(){
     var vbruto = document.querySelector(".vbruto").value;
     var vdesc = document.querySelector(".vdesc").value;
 
-    //var c = vsal;
-
     var virrf= (vbruto * 0.015);
     var vcofins = (vbruto * 0.030);
     var vcsll = (vbruto * 0.010);
@@ -13,9 +11,8 @@ function calcSal(){
 
     var vliq = (vbruto - virrf - vcofins - vcsll - vpis - vdesc);
 
-    var dados = "<span class='dados'><span> Valor Bruto: <em>R$ " + vbruto + "</em></span><span>Outros Descontos: <em>" + vdesc + "</em></span><span> Valor Líquido: R$ " + vliq + "</span></span>";
+    var dados = "<span class='dados'><span> Valor Bruto: <em>R$ " + vbruto + "</em></span><span>Outros Descontos: <em>" + vdesc + "</em></span><span> Valor Líquido: R$ " + vliq.toFixed(2) + "</span></span>";
 
     saida.innerHTML = ` <span><b>Imposto Retido: </b> <span class='rs flex cem'><b>IRRF........: </b>R\$ ${virrf.toFixed(2)}</span><span class='rs flex cem'><b>COFINS...: </b>R\$ ${vcofins.toFixed(2)}</span><span class='rs flex cem'><b>CSLL.......: </b>R\$ ${vcsll.toFixed(2)}</span><span class='rs flex cem'><b>PIS...........: </b></b>R\$ ${vpis.toFixed(2)}</span></span>${dados}`;
-    //saida.innerHTML = " <span><b>Resultado: </b> <span class='rs flex cem'><b>Diária </b>R$ "  + vdia.toFixed(2) + "</span><span class='rs flex cem'> <b>Valor por Hora: </b>R$ " + vh.toFixed(2) + "</span></span>" + dados;
-    //windows.alert(c);
+    
 };
